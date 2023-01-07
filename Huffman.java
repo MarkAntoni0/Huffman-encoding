6e5abb7e7dedd92a77c7be866b340c184c344871
Mark Tharwat <61565042+MarkAntoni0@users.noreply.github.com>
2023-01-07 21:14:42 +0200

    Add files via upload

---
+++ Huffman.java
@@ -0,0 +1,145 @@
+//////////////////////////////////////////////////////////
+// This code aims to encode using Huffman encoding      //
+// Written by: Mark Tharwat - 19200164                  //
+// Eng.mark.antonio@icloud.com                          //
+// https://www.linkedin.com/in/mark-antonio-b2777310a/  //
+// Feel free to use or edit the code                    //
+// No copy rights                                       //
+//////////////////////////////////////////////////////////
+package Code;
+
+import java.util.PriorityQueue;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Huffman {
+
+//	The printFinalCode function aims to peint the encoding per character using 
+//	Recursion. 
+//	The function takes the root node which was generated using the main function 
+//	and aims to traverse the nodes in order to generate the encoding.
+//	The String s aims to save the encoding of the last recursion 
+//	while entering the next recursion
+	public static void printFinalCode(Node rootNode, String s) {
+		if (rootNode.left == null && rootNode.right == null && Character.isLetter(rootNode.c)) {
+			System.out.println(rootNode.c + " > " + s);
+			return;
+		} else {
+			printFinalCode(rootNode.left, s + "0");
+			printFinalCode(rootNode.right, s + "1");
+		}
+	}
+
+//	The main function would start it's work by generating two arraylists to extract the chracters from the text
+//	file into them. 
+//	The variable n aims to keep track of the unique chracters found.
+//  The reason why we use arrayList instead of array to receive valeus from the text is the remocal of LF character	
+	
+	public static void main(String[] args) throws IOException {
+		int n = 0;
+
+		ArrayList<Character> characters = new ArrayList<Character>();
+		ArrayList<Integer> frequencies = new ArrayList<Integer>();
+		
+//		This path can be changes to match the text file you would like to encode 
+		Path filePath = Path.of("/home/ironhide/eclipse-workspace/HuffmanFinalProject/src/Code/characterSet.txt");
+		String content = Files.readString(filePath);
+
+//		The following lines of code aims to find the frequency of each character in the
+//		given text while placing the repeating characters to 0 to avoid duplication.
+		int[] freq = new int[content.length()];                          // freq aims to keep track of the frequency of charactes
+		System.out.println("The entered string is " + content);          // for debugging purposes
+		
+		char str1[] = content.toCharArray();                             // Convert the given string into character array
+		for (int i = 0; i < str1.length; i++) {
+			freq[i] = 1;
+			for (int j = i + 1; j < content.length(); j++) {             // Traversing what is after the current character
+				if (str1[i] == str1[j]) {
+					freq[i]++;
+					str1[j] = '0';                                       // Set str1[j] to 0 to avoid printing visited character
+				}
+			}
+		}
+		// Displays the characters and their corresponding frequency
+		System.out.println("Characters     frequencies");
+		for (int i = 0; i < freq.length - 1; i++) {                      // -1 for the lf character
+			if (str1[i] != ' ' && str1[i] != '0') {
+				System.out.println(str1[i] + "              " + freq[i]);
+				n++;
+				characters.add(str1[i]);                                 // inserting the character into array list
+				frequencies.add(freq[i]);                                // inserting the frequency into array list
+			}
+		}
+
+		System.out.println("Total unique character= " + (n));
+		System.out.println("");
+
+		System.out.println(Arrays.deepToString(characters.toArray()));  // for debugging purposes
+		System.out.println(Arrays.deepToString(frequencies.toArray())); // for debugging purposes
+		System.out.println("");
+
+		char[] charArray = new char[characters.size()];                 // preparing array to receive array list values
+		int[] charfreq = new int[frequencies.size()];                   // preparing array to receive array list values
+
+		for (int i = 0; i < n; i++) {                                   // looping over each element and adding them to array
+			charArray[i] = characters.get(i);
+			charfreq[i] = frequencies.get(i);
+		}
+
+//		The priority queue rearrange itself based on a comparison criteria which is given through the comparison
+		PriorityQueue<Node> q = new PriorityQueue<Node>(n, new ComparisonCriteria());
+
+		
+		for (int i = 0; i < n; i++) {                                   // Adding all created nodes to the queue
+			Node newCreatedNode = new Node();
+			newCreatedNode.c = charArray[i];
+			newCreatedNode.frequency = charfreq[i];
+			newCreatedNode.left = null;
+			newCreatedNode.right = null;
+
+			q.add(newCreatedNode);
+		}
+
+		Node rootNode = null;                                           // Grouping the nodes together to the roor
+		while (q.size() > 1) {
+
+			Node x = q.peek();                                          // Picking the first node with removal
+			q.poll();                                                   // Moving the queue a step ahead
+			Node y = q.peek();                                          // Picking the first node with removal
+			q.poll();                                                   // Moving the queue a step ahead
+
+			Node f = new Node();                                        // Creating a new root for the popped two nodes
+			f.frequency = x.frequency + y.frequency;
+			f.c = '*';
+
+			f.left = x;
+			f.right = y;
+
+			rootNode = f;
+			q.add(f);
+		}
+
+		
+		System.out.println("");
+		System.out.println("Encoding");
+		printFinalCode(rootNode, "");
+	}
+}
+
+class Node {
+	int frequency;
+	char c;
+
+	Node left;
+	Node right;
+}
+
+class ComparisonCriteria implements Comparator<Node> {
+	public int compare(Node x, Node y) {
+		return x.frequency - y.frequency;
+	}
+}
